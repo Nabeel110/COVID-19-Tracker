0e72354641b8c711fe2e12066aaae9ce70d33bb8
Nabeel Ahmed <nabeelmoolji1@gmail.com>
2020-06-11 14:34:39 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//importing components using new-syntax (create index.js on components folder)
+import {Cards, Chart, CountryPicker, Footer} from './components';
+import {fetchData} from './API/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// importing styles.css for App
+import styles from './App.module.css';
+
+// importing images
+
+import coronaImage from './images/covid19.png';
+import fightCorona from './images/covid.png';
+
+class App extends React.Component {
+
+  state = {
+    data : {},
+    country: '',
+  }
+
+  
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    // console.log(fetchedData);
+    this.setState({data});
+    
+    
+  }
+
+
+  handleCountryChange = async (country) => {
+
+    const data = await fetchData(country);
+
+    this.setState({data, country: country});
+    
+    
+      //fetch the data
+      // get the state
+  }
+  
+  render() {
+    const { data, country }  = this.state;
+
+    return(
+    <div className = {styles.container}>
+    <img src ={coronaImage} className = {styles.image} alt ="COVID-19" />
+    <img src ={fightCorona} className = {styles.covidFight} alt ="Fight-COVID-19" />
+      <Cards data = {data} />
+      <CountryPicker handleCountryChange = {this.handleCountryChange}/>
+      <Chart data = {data} country = {country}/>
+      <Footer />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
